3ceed10cd80518e91ed38d21896f11af7fb7dd4e
Tristan <tristancarter2772@gmail.com>
2025-09-05 22:28:01 -0500

    started home view

--- src/components/StickyHeader.vue
+++ src/components/StickyHeader.vue
@@ -6,15 +6,17 @@ import { RouterLink } from 'vue-router'
 <template>
   <header>
     <!-- logo -->
-    <router-link to="/"><img src="/tc-logo.png" alt="Tristan Carter Logo" /></router-link>
-    <nav>
-      <!-- links -->
-      <router-link to="/projects">Projects</router-link>
-      <router-link to="/blog">Blog</router-link>
-      <router-link to="/contact">Contact</router-link>
-      <router-link to="/about">About</router-link>
-      <button>Themes</button>
-    </nav>
+    <div class="container">
+      <router-link to="/"><img src="/tc-logo.png" alt="Tristan Carter Logo" /></router-link>
+      <nav>
+        <!-- links -->
+        <router-link to="/projects">Projects</router-link>
+        <router-link to="/blog">Blog</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <router-link to="/about">About</router-link>
+        <button>Themes</button>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -24,46 +26,54 @@ import { RouterLink } from 'vue-router'
 header {
   width: 100%;
   background-color: var(--content-bg);
-  display: flex;
-  align-items: flex-end;
-  justify-content: flex-start;
-  gap: var(--md-gap);
   padding: var(--md-gap);
+  position: sticky;
+  top: 0;
+  left: 0;
 
-  a img {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    border: 1px solid white;
-    box-shadow: 0 0 16px var(--primary);
-  }
-
-  nav {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-end;
-    justify-content: space-around;
-    width: 75%;
+  .container {
+    width: var(--content-width);
     margin: 0 auto;
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-start;
+    gap: var(--md-gap);
 
-    a,
-    button {
-      font-size: var(--fs-sm);
-      list-style: none;
-      margin: var(--sm-gap);
-      color: var(--txt-color);
-      text-decoration: none;
-      transition: var(--transition);
-      cursor: pointer;
-      background-color: transparent;
-      border: none;
-      border-bottom: 2px solid transparent;
+    a img {
+      width: 75px;
+      height: 75px;
+      border-radius: 50%;
+      border: 1px solid white;
+      box-shadow: 0 0 16px var(--primary);
     }
 
-    a:hover,
-    button:hover {
-      border-bottom: 2px solid var(--primary);
-      color: var(--primary);
+    nav {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-end;
+      justify-content: space-around;
+      width: 75%;
+      margin: 0 auto;
+
+      a,
+      button {
+        font-size: var(--fs-sm);
+        list-style: none;
+        margin: var(--sm-gap);
+        color: var(--txt-color);
+        text-decoration: none;
+        transition: var(--transition);
+        cursor: pointer;
+        background-color: transparent;
+        border: none;
+        border-bottom: 2px solid transparent;
+      }
+
+      a:hover,
+      button:hover {
+        border-bottom: 2px solid var(--primary);
+        color: var(--primary);
+      }
     }
   }
 }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,78 @@
 
 <template>
   <main>
-    <h2>This is the home page</h2>
+    <!-- ---------------- Home --------------------- -->
+    <section id="home">
+      <img src="/tristan-carter.jpeg" alt="" />
+      <div id="home-details">
+        <h1>Hey, I'm Tristan Carter.</h1>
+        <h2>I am a <span>web developer</span></h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad nisi distinctio aspernatur
+          quia velit consequuntur assumenda sint, facilis neque illo incidunt quasi laudantium
+          temporibus porro.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, doloribus. Molestias cum
+          quibusdam numquam quae?
+        </p>
+      </div>
+    </section>
+    <!-- ------------------- Projects --------------- -->
+    <section id="home-projects">
+      <h2>Some Projects</h2>
+      <div id="projects-card-container">
+        <div class="project-card"><h3>Project 1</h3></div>
+        <div class="project-card"><h3>Project 2</h3></div>
+        <div class="project-card"><h3>Project 3</h3></div>
+      </div>
+    </section>
   </main>
 </template>
+
+<style scoped>
+@import '../assets/base.css';
+main {
+  width: 100%;
+}
+
+#home {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: var(--content-width);
+  margin: var(--md-gap) auto;
+  gap: var(--lg-gap);
+  img {
+    aspect-ratio: 1/1;
+    height: 70dvh;
+    border-radius: 50%;
+  }
+  #home-details {
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 42px;
+      margin-bottom: var(--lg-gap);
+    }
+    h2 {
+      margin-bottom: var(--md-gap);
+      span {
+        color: var(--primary);
+      }
+    }
+    p {
+      line-height: var(--lh2);
+      margin-bottom: var(--sm-gap);
+    }
+  }
+}
+
+#home-projects {
+  background-color: var(--bg2);
+  width: var(--content-width);
+  margin: var(--md-gap) auto;
+}
+</style>
